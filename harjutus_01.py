55db32da2537802a59777c7a4fab2e6db056b8fa
madis2 <98737015+madis2@users.noreply.github.com>
2022-01-31 11:29:22 +0200

    Add files via upload

---
+++ harjutus_01.py
@@ -0,0 +1,45 @@
+# Harjutus 01
+# Matis Russi - IT-21 - HKHK
+#31.01.22
+
+#Siit algab ülesanne 1
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^')
+
+külg = int(input("Sisesta ruudu külje pikkus: "))
+pindala = külg*külg
+print("Vastus: ruudu pindala on",pindala ,"cm^2")
+
+#Harjutamise asjad
+
+'''
+nimi = input("Lisa siia oma nimi:")
+print ("Tere", nimi+"!") # + ei lisa tühikut
+vanus = int(input("Lisa siia oma vanus:"))
+vanuste_vahe = 17-vanus
+print("Meie vanuse vahe on:",vanuste_vahe,"aastat")
+'''
+
+'''
+a = 12
+b = 18
+s=a*b
+print (s)
+p = 2*(a+b)
+print ("pindala",s,"ruutmeetrit")
+print ("ümbermõõt",p,"meetrit")
+'''
+
+'''
+print ("olamiamigos") #sõne, string
+print (31012022) #int / täisarv
+print (31.5) #float / komaarv
+print ('ütle "tere maailm"')
+print("ütle \"tere maailm\"") #escape string
+print(type(33)) #näitab andmetüüpi
+'''
