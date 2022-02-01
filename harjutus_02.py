e7dca6562f9a9ccf587201f362954884eb0ba837
madis2 <98737015+madis2@users.noreply.github.com>
2022-02-01 13:45:50 +0200

    Update harjutus_02.py

--- harjutus_02.py
+++ harjutus_02.py
@@ -2,16 +2,14 @@
 # Matis Russi
 # 01.02.2022
 
-'''
 #Kütusekulu arvutamine
 
 tank = int(input("Sisesta tangitud kütus liitrites: "))
 km = int(input("Sisesta läbitud kilomeetrid: "))
 valem = round((tank/km)*100)
 print("Sinu auto kasutab",valem,"liitrit 100'le km'le")
-'''
 
-'''
+
 #Arvusüsteemid
 
 tais = int(input("Sisesta täisarv: "))
@@ -19,9 +17,8 @@ kahend = bin(tais)
 kuuend = hex(tais)
 print("Kahendsüsteemis on arv:", kahend)
 print("Kuusteistkümnendsüsteemis on arv:", kuuend)
-'''
 
-'''
+
 #Ajateisendus
 
 sis = int(input("Sisesta minutid: "))
@@ -29,9 +26,8 @@ tund = 60
 tunnid = sis // tund
 min = sis % tund
 print(tunnid,"h",":",min,"min")
-'''
 
-'''
+
 #Kolmnurga hüpotenuus
 
 a = 16
@@ -40,18 +36,16 @@ teoreem = pow(a,2)+pow(b,2)
 import math
 hypotenuus = round(math.sqrt(teoreem))
 print("Kolnurga hüpotenuus on",hypotenuus)
-'''
 
-'''
+
 #Rulluisutajad
 
 avg = 29.9
 aeg = 0.4
 vahemaa = round((avg*aeg))
 print("24 minutiga jõuab",vahemaa,"kilomeetri kaugusele")
-'''
 
-'''
+
 #pitsa
 
 sobrad = 3
@@ -59,17 +53,15 @@ hind = 12.90
 tip = 0.1
 proge = ((hind*tip)+hind)/sobrad
 print("Iga sõber maksab", proge,"€")
-'''
 
-'''
+
 #kolmnurga ümbermõõt
 
 a,b,c = 15,16,18
 p = a+b+c
 print("Kolmnurga ümbermõõt: ",p)
-'''
 
-'''
+
 #3 toote hind
 
 hind = 36.75
@@ -77,4 +69,3 @@ ale = 0.4
 kogus =3
 summa = (hind-(hind*ale))*kogus
 print(kogus, "toote hind on" ,summa,"€")
-'''
