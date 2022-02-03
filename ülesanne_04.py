3477328184b7f29838acf187025b013fa6d1b2b2
matis <98737015+madis2@users.noreply.github.com>
2022-02-03 17:40:20 +0200

    Add files via upload

---
+++ ülesanne_04.py
@@ -0,0 +1,47 @@
+# Matis Russi
+# IT-21
+# 03.02.2022
+
+'''
+#Ruut
+
+print("Kontrollime kas su kujund on ruut või ei.")
+a = int(input("Sisesta kujundi külg: "))
+b = int(input("Sisesta kujundi teine külg: "))
+if a == b:
+    print ("Sinu kujund on ruut.")
+else:
+    print("Sinu kujund ei ole ruut.")
+'''
+
+'''
+#Matemaatika
+
+c = int(input("Sisesta 1. täisarv: "))
+d = int(input("Sisesta 2. täisarv: "))
+tehe = input("Sisesta mis tehte soovid teha (+-*/): ")
+
+if tehe=="+":
+    v = c+d
+elif tehe=="-":
+    v = c-d
+elif tehe=="*":
+    v = c*d
+else:
+    v = c/d
+print(f"{c}{tehe}{d}={v}")
+'''
+
+'''
+#Juubel
+
+syn = int(input("Sisesta oma sünniaasta: "))
+yr = 2022
+age = yr-syn
+age = age/5
+if age == int(age):
+    print("Sul on juubel.")
+else:
+    print("Sul ei ole juubel.")
+'''
+
