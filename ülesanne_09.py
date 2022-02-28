3ee605acb512d6922c7591ab59942933a53586ed
matis <98737015+madis2@users.noreply.github.com>
2022-02-28 13:00:06 +0200

    Update ülesanne_09.py

--- ülesanne_09.py
+++ ülesanne_09.py
@@ -1,10 +1,12 @@
 # Ülesanne 09
 # Matis Russi
 # IT-21
-# 17.02.2022
+# 28.02.2022
 
-# Kuupäev
+# Moodulid
 
+import locale
+from dateutil.relativedelta import relativedelta
 import datetime
 
 # Inglise keeles
@@ -13,16 +15,25 @@ aeg = datetime.datetime.now()
 print(aeg.strftime("%d. %B %Y"))
 
 print('─────────────────')
+
 # Eesti keeles
 
-import locale
 locale.setlocale(locale.LC_TIME, 'et_ET.UTF-8')
 
 aeg1 = datetime.datetime.now()
 print(aeg1.strftime('%d. %B %Y'))
 
+print('─────────────────')
+
 # Isikukood
 
-pic = int(input('Sisesta oma isikukood: '))
-pic[1]
+ik = "50503310866"
+aasta = int(ik[1]+ik[2])+2000
+kuu = int(ik[4])
+paev = int(ik[5]+ik[6])
 
+alg = datetime.datetime(aasta,kuu,paev)
+lopp = datetime.datetime(int(aeg1.strftime('%Y')),int(aeg1.strftime('%m')),int(aeg1.strftime('%d')))
+
+diff = relativedelta(lopp, alg).years
+print(diff)
